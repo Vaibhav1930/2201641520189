91bb4cec2fc3baa0d605133a91a9b8a01fef219e
unknown <vaibhavmishra71930@gmail.com>
2025-09-08 14:39:58 +0530

    done

---
+++ logging-middleware/config.js
@@ -0,0 +1,8 @@
+ {
+  email: 'vaibhavmishra71930@gmail.com',
+  name: 'vaibhav mishra',
+  rollNo: '220164520189',
+  accessCode: 'sAWTuR',
+  clientID: '9cb59a27-8b01-41e3-89a0-7ba11ede1860',
+  clientSecret: 'WHTWSQfwFpNexMEF'
+}
---
+++ logging-middleware/register.js
@@ -0,0 +1,26 @@
+async function register() {
+  const response = await fetch("http://20.244.56.144/evaluation-service/register", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+email: "Vaibhavmishra71930@gmail.com",
+name: "Vaibhav Mishra",
+mobileNo:"8957071930",
+githubUsername: "githubVaibhav1930",
+rollNo: "220164520189",
+accessCode: "sAWTuR"
+}),
+  });
+
+  const text = await response.text(); // get raw response
+  console.log("Raw response:", text);
+
+  try {
+    const data = JSON.parse(text);
+    console.log("Parsed JSON:", data);
+  } catch (err) {
+    console.error("Response is not JSON, maybe an error page.");
+  }
+}
+
+register();
